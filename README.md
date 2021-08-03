95c8f5896c145074347c3f819201e4304c1f46a4
unknown <nani.jimenez@hotmail.com>
2021-08-03 16:56:01 -0500

    c.group

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # javascript
+
+...
+
+## 1: Figuras geometricas
+
+-Primer paso: definir formulas
+-Segundo: Implementación fórmulas Js
+-Cuarto: Integración con html
---
+++ figures.js
@@ -0,0 +1,26 @@
+//Código cuadrado
+console.group("Cuadrados")
+const  ladoCuadrado = 5;
+console.log("lados del cuadrado mide: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("perimetro del cuadrado mide: " + perimetroCuadrado + "cm2" );
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("área del cuadrado mide: " + areaCuadrado + "cm2");
+console.groupEnd()
+//Código de triángulo
+console.group("Triángulos")
+const ladoTriangulo1=6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log('los lados del triangulo miden: '
++ ladoTriangulo1 
++"cm, "
++ ladoTriangulo2 
++"cm, "
++ baseTriangulo
++"cm, "
+)
+console.groupEnd()
